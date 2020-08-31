787f48a51a40ad8d92b5fe17f1379c977a2ab7b2
codebloded <rohanrajput16.phy@gmail.com>
2020-08-31 19:47:31 +0530

    final commit to darkmode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React ,{useState} from 'react';
+
+
 import './App.css';
 
 function App() {
+  const [dark , setDark] = useState(false);
+  const changeHandler = ()=>{
+    setDark(!dark);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={!dark?"App":"App dark-mode"}>
+     <nav className="navbar customNav d-flex justify-content-center">
+        <label class="switch">
+            
+            <input
+             type="checkbox"
+             onChange={changeHandler}
+             />
+            <span class="slider round"></span>
+          
+        </label>  
+     </nav>
+
+      <div className="content">
+  <h1>Welcome to {dark?"Dark Mode":"Light Mode"}</h1>
+        <h1>Hey! ,This is Codebloded!</h1>
+        <h4>To switch into dark mode , click on the toogle button Above :)</h4>
+        <button className="btn btn-primary"><a className="a " href="https://codebloded.github.io/weather-app/">Go to Weather App</a></button>
+
+      </div>
     </div>
   );
 }
